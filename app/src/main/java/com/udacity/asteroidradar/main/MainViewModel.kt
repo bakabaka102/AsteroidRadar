02cd8a9b45e329430f11cb852aa373ea35a536ce
H Ngg <hnn@gmail.com>
2024-03-09 13:06:08 +0700

    - Call retrofit show image P2

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -3,20 +3,11 @@ package com.udacity.asteroidradar.main
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.udacity.asteroidradar.Constants
-import com.udacity.asteroidradar.api.NasaAsteroidAPI
 import com.udacity.asteroidradar.db.AsteroidDatabase
-import com.udacity.asteroidradar.db.AsteroidEntity
 import com.udacity.asteroidradar.db.PictureOfDayEntity
-import com.udacity.asteroidradar.models.PictureOfDay
-import com.udacity.asteroidradar.models.convertPictureToPictureDB
 import com.udacity.asteroidradar.repository.AsteroidRepository
-import com.udacity.asteroidradar.utils.Logger
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -24,9 +15,6 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val repository = AsteroidRepository(database)
 
     init {
-        /*viewModelScope.launch {
-            //loadPictureOfDay()
-        }*/
         viewModelScope.launch {
             //loadPictureOfDay()
             repository.loadPictureOfDay()
@@ -34,20 +22,5 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         }
     }
 
-    private val _pictureOfDay = MutableLiveData<PictureOfDayEntity>()
-    val pictureOfDay: LiveData<PictureOfDayEntity> get() = repository.pictureOfDay
-
-    private suspend fun loadPictureOfDay() {
-        withContext(Dispatchers.IO) {
-            try {
-                _pictureOfDay.postValue(
-                    NasaAsteroidAPI.retrofitService.getPictureOfDay(Constants.VALUE_OF_API_KEY).convertPictureToPictureDB().also {
-                        Logger.e("_pictureOfDay has value: $it")
-                    }
-                )
-            } catch (err: Exception) {
-                Logger.e("_pictureOfDay: ${err.message}")
-            }
-        }
-    }
+    val pictureOfDay: LiveData<PictureOfDayEntity> = repository.pictureOfDay
 }
--- app/src/main/java/com/udacity/asteroidradar/repository/AsteroidRepository.kt
+++ app/src/main/java/com/udacity/asteroidradar/repository/AsteroidRepository.kt
@@ -1,14 +1,14 @@
 package com.udacity.asteroidradar.repository
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.udacity.asteroidradar.Constants
 import com.udacity.asteroidradar.api.NasaAsteroidAPI
 import com.udacity.asteroidradar.db.AsteroidDatabase
-import com.udacity.asteroidradar.db.AsteroidEntity
 import com.udacity.asteroidradar.db.PictureOfDayEntity
+import com.udacity.asteroidradar.models.Asteroid
 import com.udacity.asteroidradar.models.PictureOfDay
 import com.udacity.asteroidradar.models.convertPictureToPictureDB
-import com.udacity.asteroidradar.utils.DateUtils
 import com.udacity.asteroidradar.utils.Logger
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -20,12 +20,7 @@ class AsteroidRepository(private val database: AsteroidDatabase) {
         val todayAsteroidList: LiveData<List<AsteroidEntity>>
             get() = database.asteroidDao.getAsteroidsDay(DateUtils.getCurrentDate())*/
 
-    val pictureOfDay: LiveData<PictureOfDayEntity>
-        get() = database.pictureOfDayDao.getPictureOfDay().also {
-            Logger.d("_pictureOfDay from db has: $it")
-            Logger.d("_pictureOfDay from db has value: ${it.value}")
-        }
-
+    val pictureOfDay: LiveData<PictureOfDayEntity> get() = database.pictureOfDayDao.getPictureOfDay()
 
     suspend fun loadPictureOfDay() {
         withContext(Dispatchers.IO) {
@@ -47,9 +42,9 @@ class AsteroidRepository(private val database: AsteroidDatabase) {
     suspend fun loadAsteroidList() {
         withContext(Dispatchers.IO) {
             try {
-                val asteroids: List<AsteroidEntity>? =
+                val asteroids: List<Asteroid> =
                     NasaAsteroidAPI.retrofitService.getAsteroids(Constants.VALUE_OF_API_KEY)
-                asteroids?.let { database.asteroidDao.updateData(it) }
+                //asteroids?.let { database.asteroidDao.updateData(it) }
                 Logger.d("data $asteroids")
 
             } catch (e: Exception) {
